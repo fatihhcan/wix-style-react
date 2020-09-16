fa8746008eb6bbdeb7c43a0517fc2678a81a112f
Chen Almagor <chenalmagor@gmail.com>
2020-09-16 11:16:51 +0300

    visual tests

--- src/AreaChart/test/AreaChart.visual.js
+++ src/AreaChart/test/AreaChart.visual.js
@@ -3,17 +3,544 @@ import { storiesOf } from '@storybook/react';
 import AreaChart from '../AreaChart';
 
 const commonProps = {
-  // use for repeated props across the tests (e.g. {buttonText: 'example'})
+  data: [
+    {
+      value: 5698,
+      label: '12/9',
+    },
+    {
+      value: 1170,
+      label: '13/9',
+    },
+  ],
+  tooltipContent: (item, index) => {
+    return [`${item.label}`, `${item.value}$ from your orders`];
+  },
 };
+
+const overMonthData = [
+  {
+    value: 5698,
+    label: '1/9',
+  },
+  {
+    value: 1170,
+    label: '2/9',
+  },
+  {
+    value: 4678,
+    label: '3/9',
+  },
+  {
+    value: 2030,
+    label: '4/9',
+  },
+  {
+    value: 3000,
+    label: '5/9',
+  },
+  {
+    value: 5698,
+    label: '6/9',
+  },
+  {
+    value: 1170,
+    label: '7/9',
+  },
+  {
+    value: 4678,
+    label: '8/9',
+  },
+  {
+    value: 2030,
+    label: '9/9',
+  },
+  {
+    value: 3000,
+    label: '10/9',
+  },
+  {
+    value: 5698,
+    label: '11/9',
+  },
+  {
+    value: 1170,
+    label: '12/9',
+  },
+  {
+    value: 4678,
+    label: '13/9',
+  },
+  {
+    value: 2030,
+    label: '14/9',
+  },
+  {
+    value: 3000,
+    label: '15/9',
+  },
+  {
+    value: 5698,
+    label: '16/9',
+  },
+  {
+    value: 1170,
+    label: '17/9',
+  },
+  {
+    value: 4678,
+    label: '18/9',
+  },
+  {
+    value: 2030,
+    label: '19/9',
+  },
+  {
+    value: 3000,
+    label: '20/9',
+  },
+  {
+    value: 5698,
+    label: '21/9',
+  },
+  {
+    value: 1170,
+    label: '22/9',
+  },
+  {
+    value: 4678,
+    label: '23/9',
+  },
+  {
+    value: 2030,
+    label: '24/9',
+  },
+  {
+    value: 3000,
+    label: '25/9',
+  },
+  {
+    value: 5698,
+    label: '26/9',
+  },
+  {
+    value: 1170,
+    label: '27/9',
+  },
+  {
+    value: 4678,
+    label: '28/9',
+  },
+  {
+    value: 2030,
+    label: '29/9',
+  },
+  {
+    value: 3000,
+    label: '30/9',
+  },
+];
+
+const overTwoMonthsData = [
+  {
+    value: 5698,
+    label: '1/9',
+  },
+  {
+    value: 1170,
+    label: '2/9',
+  },
+  {
+    value: 4678,
+    label: '3/9',
+  },
+  {
+    value: 2030,
+    label: '4/9',
+  },
+  {
+    value: 3000,
+    label: '5/9',
+  },
+  {
+    value: 5698,
+    label: '6/9',
+  },
+  {
+    value: 1170,
+    label: '7/9',
+  },
+  {
+    value: 4678,
+    label: '8/9',
+  },
+  {
+    value: 2030,
+    label: '9/9',
+  },
+  {
+    value: 3000,
+    label: '10/9',
+  },
+  {
+    value: 5698,
+    label: '11/9',
+  },
+  {
+    value: 1170,
+    label: '12/9',
+  },
+  {
+    value: 4678,
+    label: '13/9',
+  },
+  {
+    value: 2030,
+    label: '14/9',
+  },
+  {
+    value: 3000,
+    label: '15/9',
+  },
+  {
+    value: 5698,
+    label: '16/9',
+  },
+  {
+    value: 1170,
+    label: '17/9',
+  },
+  {
+    value: 4678,
+    label: '18/9',
+  },
+  {
+    value: 2030,
+    label: '19/9',
+  },
+  {
+    value: 3000,
+    label: '20/9',
+  },
+  {
+    value: 5698,
+    label: '21/9',
+  },
+  {
+    value: 1170,
+    label: '22/9',
+  },
+  {
+    value: 4678,
+    label: '23/9',
+  },
+  {
+    value: 2030,
+    label: '24/9',
+  },
+  {
+    value: 3000,
+    label: '25/9',
+  },
+  {
+    value: 5698,
+    label: '26/9',
+  },
+  {
+    value: 1170,
+    label: '27/9',
+  },
+  {
+    value: 4678,
+    label: '28/9',
+  },
+  {
+    value: 2030,
+    label: '29/9',
+  },
+  {
+    value: 3000,
+    label: '30/9',
+  },
+
+  {
+    value: 5698,
+    label: '1/10',
+  },
+  {
+    value: 1170,
+    label: '2/10',
+  },
+  {
+    value: 4678,
+    label: '3/10',
+  },
+  {
+    value: 2030,
+    label: '4/10',
+  },
+  {
+    value: 3000,
+    label: '5/10',
+  },
+  {
+    value: 5698,
+    label: '6/10',
+  },
+  {
+    value: 1170,
+    label: '7/10',
+  },
+  {
+    value: 4678,
+    label: '8/10',
+  },
+  {
+    value: 2030,
+    label: '9/10',
+  },
+  {
+    value: 3000,
+    label: '10/10',
+  },
+  {
+    value: 5698,
+    label: '11/10',
+  },
+  {
+    value: 1170,
+    label: '12/10',
+  },
+  {
+    value: 4678,
+    label: '13/10',
+  },
+  {
+    value: 2030,
+    label: '14/10',
+  },
+  {
+    value: 3000,
+    label: '15/10',
+  },
+  {
+    value: 5698,
+    label: '16/10',
+  },
+  {
+    value: 1170,
+    label: '17/10',
+  },
+  {
+    value: 4678,
+    label: '18/10',
+  },
+  {
+    value: 2030,
+    label: '19/10',
+  },
+  {
+    value: 3000,
+    label: '20/10',
+  },
+  {
+    value: 5698,
+    label: '21/10',
+  },
+  {
+    value: 1170,
+    label: '22/10',
+  },
+  {
+    value: 4678,
+    label: '23/10',
+  },
+  {
+    value: 2030,
+    label: '24/10',
+  },
+  {
+    value: 3000,
+    label: '25/10',
+  },
+  {
+    value: 5698,
+    label: '26/10',
+  },
+  {
+    value: 1170,
+    label: '27/10',
+  },
+  {
+    value: 4678,
+    label: '28/10',
+  },
+  {
+    value: 2030,
+    label: '29/10',
+  },
+  {
+    value: 3000,
+    label: '30/10',
+  },
+  {
+    value: 3000,
+    label: '31/10',
+  },
+];
+
+const overWeekData = [
+  {
+    value: 5698,
+    label: '1/9',
+  },
+  {
+    value: 1170,
+    label: '2/9',
+  },
+  {
+    value: 4678,
+    label: '3/9',
+  },
+  {
+    value: 2030,
+    label: '4/9',
+  },
+  {
+    value: 3000,
+    label: '5/9',
+  },
+  {
+    value: 3000,
+    label: '6/9',
+  },
+  {
+    value: 3000,
+    label: '7/9',
+  },
+];
+
+const nonCompactOnYAxisData = [
+  {
+    value: 1500,
+    label: '12/9',
+  },
+  {
+    value: 117,
+    label: '13/9',
+  },
+  {
+    value: 200,
+    label: '14/9',
+  },
+  {
+    value: 159,
+    label: '15/9',
+  },
+  {
+    value: 300,
+    label: '16/9',
+  },
+];
 
 const tests = [
   {
-    describe: 'sanity', // prop name (e.g. size)
+    describe: 'minimal data (2 data points)',
     its: [
       {
-        it: 'default', // prop variation (e.g. small)
+        it: 'default maxTicksLimit',
+        props: {},
+      },
+      {
+        it: 'maxTicksLimit: 10',
         props: {
-          // the simulation (e.g. {size: "small"})
+          maxTicksLimit: 10,
+        },
+      },
+    ],
+  },
+  {
+    describe: 'over week data (7 data points)',
+    its: [
+      {
+        it: 'default maxTicksLimit',
+        props: {
+          data: overWeekData,
+        },
+      },
+      {
+        it: 'maxTicksLimit: 10',
+        props: {
+          data: overWeekData,
+          maxTicksLimit: 10,
+        },
+      },
+    ],
+  },
+  {
+    describe: 'over month data (30 data points)',
+    its: [
+      {
+        it: 'default maxTicksLimit',
+        props: {
+          ...commonProps,
+          data: overMonthData,
+        },
+      },
+      {
+        it: 'maxTicksLimit: 10',
+        props: {
+          data: overMonthData,
+          maxTicksLimit: 10,
+        },
+      },
+    ],
+  },
+  {
+    describe: 'over 2 months data (61 data points)',
+    its: [
+      {
+        it: 'default maxTicksLimit',
+        props: {
+          ...commonProps,
+          data: overTwoMonthsData,
+        },
+      },
+      {
+        it: 'maxTicksLimit: 10',
+        props: {
+          data: overTwoMonthsData,
+          maxTicksLimit: 10,
+        },
+      },
+    ],
+  },
+  {
+    describe: 'combining non-compact numbers on y axis',
+    its: [
+      {
+        it: 'default maxTicksLimit',
+        props: {
+          data: nonCompactOnYAxisData,
+        },
+      },
+      {
+        it: 'maxTicksLimit: 10 ',
+        props: {
+          maxTicksLimit: 10,
+          data: nonCompactOnYAxisData,
+        },
+      },
+    ],
+  },
+  {
+    describe: 'no tooltips',
+    its: [
+      {
+        it: 'default maxTicksLimit',
+        props: {
+          ...commonProps,
+          tooltipContent: null,
         },
       },
     ],
